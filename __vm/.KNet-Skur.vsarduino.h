25ea29adbbc42f11a1d0ff21a233cdecc24c79e5
LarsKjeldsen <lars@kjeldsen.net>
2021-01-17 09:40:08 +0100

    Cleanup

--- __vm/.KNet-Skur.vsarduino.h
+++ __vm/.KNet-Skur.vsarduino.h
@@ -17,6 +17,8 @@
 #define __ESP8266_ESP8266__
 #define __ets__
 #define ICACHE_FLASH
+#define MMU_IRAM_SIZE 0x8000
+#define MMU_ICACHE_SIZE 0x8000
 #define NONOSDK22x_190703 1
 #define F_CPU 80000000L
 #define LWIP_OPEN_SRC
